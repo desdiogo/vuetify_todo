b9893b8507625941c52c390e361eb6ef455424d6
Diogo Eleutero <desdiogo10@yahoo.com>
2021-03-05 11:30:35 -0300

    layout pages e routes

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,15 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Vuetify Todo
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Lista de tarefas
-          </v-list-item-subtitle>
+          <v-list-item-title class="title"> Vuetify Todo </v-list-item-title>
+          <v-list-item-subtitle> Lista de tarefas </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -27,14 +23,41 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="tatry.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>3
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -44,8 +67,8 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-      { title: "Todo", icon: "mdi-view-dashboard" },
-      { title: "Sobre", icon: "mdi-help-box" },
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "Sobre", icon: "mdi-help-box", to: "/about" },
     ],
   }),
 };
